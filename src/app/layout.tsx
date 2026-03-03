73f247d1f598be1c238b2175ffc2fb349afbee9d
Mohammed Abdul Rafey Khan <krafey981@gmail.com>
2026-03-03 17:25:06 +0530

    Navbar styled for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
 import ReduxProvider from "@/store/provider";
 import "./globals.css";
-import { Navbar } from "@/components/Navbar";
+import Navbar from "@/components/Navbar"
+import { getUser } from "@/helpers/getCurrentUser";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getUser();
+  const plainUser = {
+    ...user.toObject(),
+    _id: user._id.toString(),
+  };
   return (
     <html lang="en">
       <body>
         <ReduxProvider>
-          <Navbar />
+          <Navbar user={plainUser} />
           {children}
         </ReduxProvider>
       </body>
